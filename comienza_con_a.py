938fb73e3a500ede789624985decf378964962fc
Moni-ib <monica.ibarra@iteso.mx>
2023-10-13 08:36:05 -0600

    p1

--- comienza_con_a.py
+++ comienza_con_a.py
@@ -3,13 +3,18 @@ Inserta el encabezado aquí y escribe tu código abajo
 """
 
 # Declaraciones
-CONSTANTE = valor
+CONSTANTE = "A"
+CONSTANTE_2 = "Á"
 
 # Entradas
-entrada = input()
+entrada = input("Escribe una palabra: ")
 
 # Proceso
+entrada = entrada.upper()
+if entrada[0] in CONSTANTE:
+    print(f"'{entrada}' comienza con '{CONSTANTE}'")
+elif entrada[0] in CONSTANTE_2:
+    print(f"'{entrada}' comienza con '{CONSTANTE_2}'")
+else:
+     print(f"'{entrada}' no comienza con 'A'")
 
-
-# Salidas
-print(salida)
