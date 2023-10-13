5b684df39ca45aa17106bb08678723af5a14d94a
Moni-ib <monica.ibarra@iteso.mx>
2023-10-13 08:40:20 -0600

    CONSTANTE

--- comienza_con_a.py
+++ comienza_con_a.py
@@ -3,8 +3,7 @@ Inserta el encabezado aquí y escribe tu código abajo
 """
 
 # Declaraciones
-CONSTANTE = "A"
-CONSTANTE_2 = "Á"
+CONSTANTE = "AÁ"
 
 # Entradas
 entrada = input("Escribe una palabra: ")
@@ -13,8 +12,6 @@ entrada = input("Escribe una palabra: ")
 entrada = entrada.upper()
 if entrada[0] in CONSTANTE:
     print(f"'{entrada}' comienza con '{CONSTANTE}'")
-elif entrada[0] in CONSTANTE_2:
-    print(f"'{entrada}' comienza con '{CONSTANTE_2}'")
 else:
     print(f"'{entrada}' no comienza con 'A'")
 
